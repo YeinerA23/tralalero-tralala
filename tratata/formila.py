0aaa0cdb6657eba186822b2117ec0764379f1116
YeinerA23 <ypalacioscasas08@gmail.com>
2025-07-16 11:48:50 -0500

    Update formila.py

--- tratata/formila.py
+++ tratata/formila.py
@@ -1,7 +1,8 @@
+import math
 a = float(input("ingese e valor de a"))
 b = float(input("ingese e valor de b"))
 c = float(input("ingese e valor de c"))
-x = (-b-(b**2-4*a*c)**0.5)/(2*a)
-x2 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+x = (-b- math.sqrt(b**2-4*a*c))/(2*a)
+x2 = (-b+ math.sqrt(b**2-4*a*c))/(2*a)
 print (x)
 print(x2)
