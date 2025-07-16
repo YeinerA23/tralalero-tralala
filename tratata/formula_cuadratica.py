a14ff5ba19bc47ce4bee8bc3209555f57f0a2390
YeinerA23 <ypalacioscasas08@gmail.com>
2025-07-16 11:58:49 -0500

    Update and rename formila.py to formula_cuadratica.py

---
+++ tratata/formula_cuadratica.py
@@ -0,0 +1,8 @@
+import math
+a = float(input("ingese e valor de a: "))
+b = float(input("ingese e valor de b: "))
+c = float(input("ingese e valor de c: "))
+x = (-b- math.sqrt(b**2-4*a*c))/(2*a)
+x2 = (-b+ math.sqrt(b**2-4*a*c))/(2*a)
+print (x)
+print(x2)
