27ff10ab950545f84c6522b90fc99e35c9ad9a9a
YeinerA23 <ypalacioscasas08@gmail.com>
2025-07-16 11:43:03 -0500

    Add files via upload

---
+++ tratata/formila.py
@@ -0,0 +1,7 @@
+a = float(input("ingese e valor de a"))
+b = float(input("ingese e valor de b"))
+c = float(input("ingese e valor de c"))
+x = (-b-(b**2-4*a*c)**0.5)/(2*a)
+x2 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+print (x)
+print(x2)
---
+++ tratata/hola.py
@@ -0,0 +1,28 @@
+#print ("hola mundo")
+#num2 = int(input("escriba un numero: "))
+#resultado = num1 + num2
+#print("el resultad es:",resultado) 
+
+
+nombre = "yeiner"
+edad = 25 
+sexo = "Hombre"
+altura = 1,80
+peso = 67
+
+print(f"su nombre es {nombre}")
+print(f"su edad es {edad}")
+print(f"su sexo es {sexo}")
+print(f"su altura es {altura}")
+print(f"su peso es {peso}")
+
+print(f"su nombre es {nombre} su edad {edad} su sexo es {sexo} su altura es { altura} su peso es {peso} kg")
+
+
+nombre1 = str(input("ingrese su nombre :" ))
+edad1 = int(input("ingrese su edad: "))
+sexo1 = str(input("ingrese su sexo: "))
+altura1 = float(input("ingrese su altura: ")) 
+peso1 = int(input("ingese su peso :"))
+
+print(f"su nombre es {nombre1} su edad {edad1} su sexo es {sexo1} su altura es { altura1} su peso es {peso1} kg")
