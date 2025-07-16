eb8981a3394f4f3e24fd1d69b9f6e1d32125d273
YeinerA23 <ypalacioscasas08@gmail.com>
2025-07-16 12:00:38 -0500

    Update formula_cuadratica.py

--- tratata/formula_cuadratica.py
+++ tratata/formula_cuadratica.py
@@ -1,8 +1,8 @@
-import math
+import cmath
 a = float(input("ingese e valor de a: "))
 b = float(input("ingese e valor de b: "))
 c = float(input("ingese e valor de c: "))
-x = (-b- math.sqrt(b**2-4*a*c))/(2*a)
-x2 = (-b+ math.sqrt(b**2-4*a*c))/(2*a)
+x = (-b- cmath.sqrt(b**2-4*a*c))/(2*a)
+x2 = (-b+ cmath.sqrt(b**2-4*a*c))/(2*a)
 print (x)
 print(x2)
