f71949a8285ae95842ea6b77046af272ba2dbf3b
YeinerA23 <ypalacioscasas08@gmail.com>
2025-07-22 16:03:24 -0500

    Update porcentajes.py

--- tratata/porcentajes.py
+++ tratata/porcentajes.py
@@ -2,4 +2,4 @@ print("tabla de porcentajes")
 porc = int(input("ingrese el porcentaje que desea calcular: "))
 cant = int(input("ingrese el numero al que le desea calcular el porcentaje: "))
 resultado = porc * cant/100 
-print (resultado)
+print (f"el {porc} de {cant} es {resultado}")
